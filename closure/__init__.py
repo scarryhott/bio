241ea1ed82a04357ee765ee82ec2ed298272c9e8
scarryhott <118125945+scarryhott@users.noreply.github.com>
2026-08-04 10:13:53 -0400

    export closure architecture

---
+++ closure/__init__.py
@@ -0,0 +1,18 @@
+from .coevolution import BiologicalPerspective, CoevolutionCarrier, to_potential_gate
+from .rnd_controller import TokenAdmission, closure_token_admission
+from .runtime import ClosureRuntime
+from .types import ClosureReceipt, MicroAction, PotentialGate, Resolution, ReturnWitness
+
+__all__ = [
+    "BiologicalPerspective",
+    "CoevolutionCarrier",
+    "to_potential_gate",
+    "TokenAdmission",
+    "closure_token_admission",
+    "ClosureRuntime",
+    "ClosureReceipt",
+    "MicroAction",
+    "PotentialGate",
+    "Resolution",
+    "ReturnWitness",
+]
